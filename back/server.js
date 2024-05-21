80ed1c7ee2b9ca64586b163c1a75eef8c2602545
“Grygorenko” <vanua.the.best@gmail.com>
2024-05-21 22:28:32 +0300

    final

--- back/server.js
+++ back/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
-const bodyParser = require('body-parser');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
